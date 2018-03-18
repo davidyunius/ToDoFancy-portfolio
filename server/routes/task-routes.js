b7746198a42a567bbce23d4060711e4551d71ae1
davidyunius <32894219+davidyunius@users.noreply.github.com>
2018-03-18 21:53:00 +0700

    checkpoint

--- server/routes/task-routes.js
+++ server/routes/task-routes.js
@@ -3,6 +3,9 @@ const router =  express.Router()
 const task = require('../controllers/task-controller')
 
 router
-    .get('/', task.viewTask)
+    .get('/list', task.viewTask)
+    .post('/add', task.addTask)
+    .delete('/delete/:id', task.deleteTask)
+    .patch('/update/:id', task.updateTask)
 
 module.exports = router
